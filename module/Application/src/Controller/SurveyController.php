1c8aa4053565a963ddee6092dd852244a990a648
Aimee Wyld <aimee.eloise@mail.com>
2016-11-29 16:58:10 +0000

    s3 Started JS for extracting survey data and sending via ajax and created controller to send to

---
+++ module/Application/src/Controller/SurveyController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class SurveyController extends AbstractActionController
+{
+    public function indexAction($data)
+    {
+        //decode JSON($data) and manipulate to fit the PDO insert
+        //validation and return for model
+    }
+}
