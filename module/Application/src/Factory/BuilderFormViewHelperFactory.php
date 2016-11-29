6ccb09dc09d6c5faf84c57e8ac1a6402c9f38d09
Jess <j.j.isaacs@hotmail.co.uk>
2016-11-29 11:37:25 +0000

    s2 remove unused viewHelper

---
+++ module/Application/src/Factory/BuilderFormViewHelperFactory.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: academy
+ * Date: 29/11/2016
+ * Time: 11:25
+ */
---
+++ module/Application/src/Helper/BuilderFormViewHelper.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Application;
+
+class BuilderFormViewHelper {
+
+    public function __invoke()
+    {
+        return 'hi';
+    }
+
+}
