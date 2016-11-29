f489822bad04404a96343b9004f985d6f71ef5a9
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2016-11-29 11:48:02 +0000

    s2 added a listener for .input-selectore and added html classes: text-input, radio-input & checkbox-input

--- public/js/surveyBuilderForm.js
+++ public/js/surveyBuilderForm.js
@@ -1,1 +1,9 @@
+$('.question-adder .input-selector').change(function() {
+    if ($('.input-selector .radio-input').prop('selected'))
+    {
 
+    } elseif ($('.input-selector .checkbox-input').prop('selected'))
+    {
+
+    }
+})
