e50db9cc10e539f1500992ae0fe71030480481a9
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2016-11-29 14:04:35 +0000

    s2 wrote handler for listener on input-selector

--- public/js/surveyBuilderForm.js
+++ public/js/surveyBuilderForm.js
@@ -1,9 +1,37 @@
-$('.question-adder .input-selector').change(function() {
-    if ($('.input-selector .radio-input').prop('selected'))
-    {
+$(function()
+{
+    $('.question-adder .input-selector').change(function() {
+        $('#question-options').remove()
+        if (!$('.input-selector .text-input').prop('selected'))
+        {
+            $('.question-adder form').append(
+            '<div class="bg-info" id="question-options">' +
+            '<h4>Options:</h4>' +
+            '<input type="text" id="option-text">' +
+            '<button class="btn" id="add-option">+</button>' +
+            '</div>'
+            )
+            $('#add-option').click(function() {
+                $(this).after(
+                    '<div>' +
+                    '<div>' + $('#option-text').val() + '</div>' +
+                    '<button class="btn remove-option">-</button>' +
+                    '</div>'
+                )
+                $('#option-text').val('')
+                $('.remove-option').click(function(){
+                    $(this).parent('div').remove()
+                })
+            })
+        } else
+        {
+        }
+    })
 
-    } elseif ($('.input-selector .checkbox-input').prop('selected'))
-    {
 
-    }
 })
+
+
+
+//$('body').css('background', 'red')
+//$('body').css('background', 'white')
