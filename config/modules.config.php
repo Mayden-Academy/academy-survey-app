ba34164a113e9a338d9fb9c44bb82ed80949d3cf
Luke Winsley <lukewinsley@gmail.com>
2016-11-29 09:26:34 +0000

    test stuffs

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,15 +11,6 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
-    'Zend\ServiceManager\Di',
-    'Zend\Session',
-    'Zend\Mvc\Plugin\Prg',
-    'Zend\Mvc\Plugin\Identity',
-    'Zend\Mvc\Plugin\FlashMessenger',
-    'Zend\Mvc\Plugin\FilePrg',
-    'Zend\Log',
-    'Zend\Form',
-    'Zend\Db',
     'Zend\Router',
     'Zend\Validator',
     'Application',
