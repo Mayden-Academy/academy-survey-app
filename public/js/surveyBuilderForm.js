3063bc1e5841fcf3b20a879d5416579918181351
Jess <j.j.isaacs@hotmail.co.uk>
2016-11-29 12:12:07 +0000

    s2 add .ready function

--- public/js/surveyBuilderForm.js
+++ public/js/surveyBuilderForm.js
@@ -1,9 +1,13 @@
-$('.question-adder .input-selector').change(function() {
-    if ($('.input-selector .radio-input').prop('selected'))
-    {
+$(function(){
 
-    } elseif ($('.input-selector .checkbox-input').prop('selected'))
-    {
+    $('.question-adder .input-selector').change(function() {
+        if ($('.input-selector .radio-input').prop('selected'))
+        {
 
-    }
+        } elseif ($('.input-selector .checkbox-input').prop('selected'))
+        {
+
+        }
+    })
+
 })
