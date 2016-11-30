203430725e18d086e7b8166ea48d424f84577903
Luke Winsley <lukewinsley@gmail.com>
2016-11-30 14:36:19 +0000

    s3 fix survey model queries and validation

--- module/Application/src/Model/SurveyModel.php
+++ module/Application/src/Model/SurveyModel.php
@@ -31,12 +31,14 @@ class SurveyModel
      */
     public function save($survey) {
        $surveyId = $this->saveSurveyDetails($survey['survey_name'], $survey['user_id']);
-
          if (is_int($surveyId) && $surveyId > 0){
              foreach($survey['questions'] as $question) {
+                 $options = $question['options'];
+                 unset($question['options']);
+
                  $questionId =  $this->saveQuestionDetails($question, $surveyId);
                  if (is_int($questionId) && $questionId > 0){
-                     foreach($question['options'] as $option) {
+                     foreach($options as $option) {
                          if(!$this->saveOptionDetails($option, $questionId)) {
                              throw new \Exception('Option save failed');
                          }
@@ -63,7 +65,7 @@ class SurveyModel
         $sql = "INSERT INTO `survey` (`name`, `creator`) VALUES (?, ?);";
         $query = $this->pdo->prepare($sql);
         if ($query->execute([$surveyName, $userId])) {
-            return $this->pdo->lastInsertId();
+            return (INT)$this->pdo->lastInsertId();
         }
         return false;
     }
@@ -79,8 +81,9 @@ class SurveyModel
         $sql = "INSERT INTO `question` (`text`, `type`, `survey_id`, `required`, `order`) VALUES (:question_text, :question_type, :survey_id, :required, :question_order);";
         $query = $this->pdo->prepare($sql);
         $questionDetails['survey_id'] = $surveyId;
+
         if ($query->execute($questionDetails)) {
-            return $this->pdo->lastInsertId();
+            return (INT)$this->pdo->lastInsertId();
         }
         return false;
     }
@@ -93,7 +96,7 @@ class SurveyModel
      * @return bool if save option worked
      */
     public function saveOptionDetails($displayValue, $questionId ) {
-        $sql = "INSERT INTO `option` (question_id`, `display_value`) VALUES (?, ?);";
+        $sql = "INSERT INTO `option` (`question_id`, `display_value`) VALUES (?, ?);";
         $query = $this->pdo->prepare($sql);
         $optionDetails['question_id'] = $questionId;
         return $query->execute([$questionId, $displayValue]);
