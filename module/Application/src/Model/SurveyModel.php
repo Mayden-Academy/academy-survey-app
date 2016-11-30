44d3520a7aa598fd404581dafb582f2fbbc54f80
Luke Winsley <lukewinsley@gmail.com>
2016-11-30 10:23:46 +0000

    s3 survey model

---
+++ module/Application/src/Model/SurveyModel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: academy
+ * Date: 30/11/2016
+ * Time: 10:19
+ */
+
+namespace Application\Model;
+
+
+class SurveyModel
+{
+    public function save($survey) {
+        //TODO save survey
+        return true;
+    }
+}
