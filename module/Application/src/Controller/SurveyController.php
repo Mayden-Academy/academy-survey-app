f519fefedb639b15509b3d143c44ef308e8280f8
Luke Winsley <lukewinsley@gmail.com>
2016-11-30 16:26:25 +0000

    docblocked methods

--- module/Application/src/Controller/SurveyController.php
+++ module/Application/src/Controller/SurveyController.php
@@ -13,16 +13,67 @@ use Zend\View\Model\JsonModel;
 
 class SurveyController extends AbstractActionController
 {
+    /**
+     * @var SurveyModel
+     */
     private $model;
 
+    /**
+     * SurveyController constructor.
+     * @param SurveyModel $model
+     */
     public function __construct(SurveyModel $model)
     {
         $this->model = $model;
     }
 
+    /**
+     * checks to see if the survey has been created
+     *
+     * @return JsonModel Json saying whether the survey had been successfully created
+     */
     public function createAction()
     {
-        $response = ['success' => $this->model->save($this->params()->fromPost())];
+        $data = $this->params()->fromPost();
+        if ($this->validateData($data)) {
+            // set user_id
+            $response = ['success' => $this->model->save($data)];
+        } else {
+            $response = ['success' => false, 'message' => 'Missing required data'];
+        }
+
         return new JsonModel($response);
     }
+
+    /**
+     * Validates whether the correct data hasd been inputted
+     *
+     * @param $data data that is being inputted
+     * @return bool if all data is present or not
+     */
+    private function validateData($data) {
+        if (
+            !empty($data['survey_name']) &&
+            !empty($data['questions']) &&
+            is_array($data['questions'])
+        ) {
+            foreach ($data['questions'] as $question) {
+                if(
+                    !empty($question['question_order']) &&
+                    !empty($question['question_text']) &&
+                    !empty($question['question_type']) &&
+                    !empty($question['required']) &&
+                    is_array($question['options']) &&
+                    count($question['options']) > 0
+                ) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
 }
