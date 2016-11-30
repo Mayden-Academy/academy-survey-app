83633d9cda1e93672388177d4518978cc81ee43f
Jess <j.j.isaacs@hotmail.co.uk>
2016-11-30 10:06:20 +0000

    removed empty line

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -60,7 +60,6 @@ return [
                             'defaults' => [ 'action' => 'post'],
                         ]
                     ],
-
                 ],
             ],
             'login' => [
