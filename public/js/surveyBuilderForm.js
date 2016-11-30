f45b03aeef9970bee213af4497ba91fe33278622
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2016-11-30 15:01:47 +0000

    s2 removed dependency on addQuestionButton in option adding functions

--- public/js/surveyBuilderForm.js
+++ public/js/surveyBuilderForm.js
@@ -3,79 +3,6 @@ $(function(){
 
     addQuestionButton.prop("disabled",true)
 
-    /**
-     * Adds the facility to add and remove options for multiple choice questions. Initially this consists of a text
-     * field and an add option (+) button.
-     *
-     * @param $container OBJECT the created DOM objects are appended to the contents of this html element
-     */
-    function addOptionsCreator($container)
-    {
-        $container.append(
-            '<div id="question-options" class="input-group">' +
-            '<label for="option-text">Options:</label>' +
-            '<br>' +
-            '<input type="text" id="option-text">' +
-            '<button class="btn input-group-addon" id="add-option">+</button>' +
-            '</div>'
-        )
-
-        createAddOptionHandler()
-    }
-
-    /**
-     * Creates a handler for the click event on the add option button. The handler adds the option (providing it's
-     * non-empty) to the list of options for that question.
-     * Then validates the form.
-     */
-    function createAddOptionHandler()
-    {
-        $('#add-option').click(function()
-        {
-            if($('#option-text').val() !== '') // disables an empty string being added as an option
-            {
-                createOption($('#option-text'))
-            }
-
-            validateNewQuestion($('#question').val(), addQuestionButton, $('#input-selector').val())
-        })
-    }
-
-    /**
-     * Creates a disabled field containing the text passed in, with a remove (-) button .
-     *
-     * @param $optionText OBJECT the text input element containing the text for the option being created
-     */
-    function  createOption($optionText)
-    {
-        $('#option-text').before(
-            '<div class="input-group">' +
-            '<input class="bg-success" value="' + $optionText.val() + '" disabled>' +
-            '<button class="btn remove-option input-group-addon">-</button>' +
-            '</div>'
-        )
-        $optionText.val('')
-
-        $('.remove-option').click(function()
-        {
-            removeOption(this)
-        })
-
-    }
-
-    /**
-     * Removes the selected option from the list and then validates the form.
-     *
-     * @param currentOption OBJECT the remove button for the option to be removed
-     */
-    function removeOption(currentOption)
-    {
-        $(currentOption).parent('div').remove()
-        validateNewQuestion($('#question').val(), addQuestionButton, $('#input-selector').val())
-    }
-
-
-
     $('#input-selector').change(function()
     {
         if ($('#input-selector').val() !== 'text-input' && $('#question-options').length < 1)
@@ -156,6 +83,84 @@ $(function(){
         }
     )
 })
+
+
+//_________________________________FUNCTION DEFINITIONS__________________________________________
+
+
+/**
+ * Adds the facility to add and remove options for multiple choice questions. Initially this consists of a text
+ * field and an add option (+) button.
+ *
+ * @param $container OBJECT the created DOM objects are appended to the contents of this html element
+ */
+function addOptionsCreator($container)
+{
+    $container.append(
+        '<div id="question-options" class="input-group">' +
+        '<label for="option-text">Options:</label>' +
+        '<br>' +
+        '<input type="text" id="option-text">' +
+        '<button class="btn input-group-addon" id="add-option">+</button>' +
+        '</div>'
+    )
+
+    createAddOptionHandler()
+}
+
+
+/**
+ * Creates a handler for the click event on the add option button. The handler adds the option (providing it's
+ * non-empty) to the list of options for that question.
+ * Then validates the form.
+ */
+function createAddOptionHandler()
+{
+    $('#add-option').click(function()
+    {
+        if($('#option-text').val() !== '') // disables an empty string being added as an option
+        {
+            createOption($('#option-text'))
+        }
+
+        validateNewQuestion($('#question').val(), $('#add-question'), $('#input-selector').val())
+    })
+}
+
+
+/**
+ * Creates a disabled field containing the text passed in, with a remove (-) button .
+ *
+ * @param $optionText OBJECT the text input element containing the text for the option being created
+ */
+function  createOption($optionText)
+{
+    $('#option-text').before(
+        '<div class="input-group">' +
+        '<input class="bg-success" value="' + $optionText.val() + '" disabled>' +
+        '<button class="btn remove-option input-group-addon">-</button>' +
+        '</div>'
+    )
+    $optionText.val('')
+
+    $('.remove-option').click(function()
+    {
+        removeOption(this)
+    })
+
+}
+
+
+/**
+ * Removes the selected option from the list and then validates the form.
+ *
+ * @param currentOption OBJECT the remove button for the option to be removed
+ */
+function removeOption(currentOption)
+{
+    $(currentOption).parent('div').remove()
+    validateNewQuestion($('#question').val(), $('#add-question'), $('#input-selector').val())
+}
 
 /**
  * Validates input of new question form
