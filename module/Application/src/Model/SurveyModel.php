628e078e92932ec8af30c4eca79350348bd43ff1
Luke Winsley <lukewinsley@gmail.com>
2016-11-30 12:10:23 +0000

    s3 docblock methods created

--- module/Application/src/Model/SurveyModel.php
+++ module/Application/src/Model/SurveyModel.php
@@ -13,11 +13,22 @@ class SurveyModel
 {
     private $pdo;
 
+    /**
+     * SurveyModel constructor.
+     * @param $pdo
+     */
     public function __construct($pdo)
     {
         $this->pdo = $pdo;
     }
 
+    /**
+     *  Save all survey information
+     *
+     * @param $survey ARRAY all survey data
+     * @return bool did the save work
+     * @throws \Exception
+     */
     public function save($survey) {
        $surveyId = $this->saveSurveyDetails($survey['survey_name'], $survey['user_id']);
 
@@ -41,6 +52,13 @@ class SurveyModel
         return true;
     }
 
+    /**
+     * Saves the survey details
+     *
+     * @param $surveyName name of survey
+     * @param $userId id of the user that created the survey
+     * @return bool/int id of the saved survey or false if the save failed
+     */
     public function saveSurveyDetails($surveyName, $userId) {
         $sql = "INSERT INTO `survey` (`name`, `creator`) VALUES (?, ?);";
         $query = $this->pdo->prepare($sql);
@@ -50,6 +68,13 @@ class SurveyModel
         return false;
     }
 
+    /**
+     * Saves the survey question details
+     *
+     * @param $questionDetails details of the questions
+     * @param $surveyId id of the survey that has been created
+     * @return bool/int id of saved question or false if the save failed
+     */
     public function saveQuestionDetails($questionDetails, $surveyId) {
         $sql = "INSERT INTO `question` (`text`, `type`, `survey_id`, `required`, `order`) VALUES (:question_text, :question_type, :survey_id, :required, :question_order);";
         $query = $this->pdo->prepare($sql);
@@ -60,6 +85,13 @@ class SurveyModel
         return false;
     }
 
+    /**
+     * Saves the survey option details
+     *
+     * @param $displayValue text of the option entered
+     * @param $questionId id of the question that the options relates too
+     * @return bool if save option worked
+     */
     public function saveOptionDetails($displayValue, $questionId ) {
         $sql = "INSERT INTO `option` (question_id`, `display_value`) VALUES (?, ?);";
         $query = $this->pdo->prepare($sql);
