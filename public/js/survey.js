05817564d032873ae5ca4c82f80261e9cba115cc
Aimee Wyld <aimee.eloise@mail.com>
2016-11-30 17:18:23 +0000

    Altered ajax success parameter and slightly refactored

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,59 +1,36 @@
 $(function() {
-
     var questionCount
-    var $saveBtn = $('#submit-btn')
     var surveyNameLength
+    var $saveBtn = $('#submit-btn')
+    var $surveyName = $('#survey-name')
     var surveyId
 
     $saveBtn.prop("disabled", true)
 
-    $('#survey-name').keyup(function() {
-        surveyNameLength = $('#survey-name').val().length
+    $surveyName.keyup(function()
+    {
+        surveyNameLength = $surveyName.val().length
         validateSurvey(surveyNameLength, questionCount)
     })
 
-    $saveBtn.click(function() {
+    $saveBtn.click(function()
+    {
         var survey = getValues()
         ajaxSurvey(survey)
     })
 
-    $('#add-question').click(function() {
+    $('#add-question').click(function() //TODO make the two below into one function?
+    {
         // updating question count for validation
         questionCount = $('#question-container .new-question').length
         validateSurvey(surveyNameLength, questionCount)
     })
 
-    $(document).on('click', '.remove-question' ,function() {
+    $(document).on('click', '.remove-question', function()
+    {
         questionCount = $('#question-container .new-question').length
         validateSurvey(surveyNameLength, questionCount)
     })
-
-    function getValues() {
-        var questions = []
-        $('.new-question').each(function(key, value) {
-            var questionOrder = key + 1
-            var questionText = $('h5', this).text()
-            var questionType = $('.options input', this).attr('type')
-            var required = $(this).data('required')
-            var options = []
-            if (questionType != 'text') {
-                $('.options input', this).each(function() {
-                    options.push($(this).val())
-                })
-            }
-            questions.push({
-                "question_order" : questionOrder,
-                    "question_text" : questionText,
-                    "question_type" : questionType,
-                    "required" : required,
-                    "options" : options
-            })
-        })
-        var survey = {
-            "survey_name": $('#survey-name').val(),
-            "questions": questions}
-        return survey;
-        }
 
     function validateSurvey(surveyNameValue, questionCount)
     {
@@ -66,19 +43,62 @@ $(function() {
             $saveBtn.prop("disabled", true)
         }
     }
-
-    function ajaxSurvey(survey, surveyId) {
-        $.ajax({
-            method: "POST",
-            url: "/survey/create",
-            data: survey,
-            success: function(response) {
-                surveyId = response['surveyId']
-                alert( "Data Saved")
-            }
-        })
-    }
 })
+
+function getValues()
+{
+    var questions = []
+
+    $('.new-question').each(function(key)
+    {
+        var questionOrder = key + 1
+        var questionText = $('h5', this).text()
+        var questionType = $('.options input', this).attr('type')
+        var required = $(this).data('required')
+        var options = []
+
+        if (questionType != 'text') {
+            $('.options input', this).each(function()
+            {
+                options.push($(this).val())
+            })
+        }
+
+        questions.push({
+            "question_order" : questionOrder,
+            "question_text" : questionText,
+            "question_type" : questionType,
+            "required" : required,
+            "options" : options
+        })
+    })
+
+    var survey = {
+        "survey_name": $('#survey-name').val(),
+        "questions": questions}
+    return survey;
+}
+
+function ajaxSurvey(survey, surveyId)
+{
+    $.ajax({
+        method: "POST",
+        url: "/survey/create",
+        data: survey,
+        success: function(response)
+        {
+            if (response['success']) {
+                surveyId = response['surveyId'] //TODO check that this actually saves
+                alert("Data Saved")
+            } else {
+                alert(response['displayMessage']) //TODO check if this is the key that is returned by controller
+            }
+
+        }
+    })
+}
+
+
 
 
 
