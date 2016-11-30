44a3e5eb12cc7806ebb8eee1b715a1cf3a835f26
Jess <j.j.isaacs@hotmail.co.uk>
2016-11-30 16:23:37 +0000

    s4 tidy SurveyControllerFactory code

--- module/Application/src/Factory/SurveyControllerFactory.php
+++ module/Application/src/Factory/SurveyControllerFactory.php
@@ -1,17 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: benmorris
- * Date: 29/11/2016
- * Time: 14:02
- */
 
 namespace Application\Factory;
+
+use \Application\Controller\SurveyController;
 
 class SurveyControllerFactory
 {
     public function __invoke($sm) {
-        $model = $sm->get(\Application\Model\SurveyModel::class);
-        return new \Application\Controller\SurveyController($model);
+        $surveyModel = $sm->get(\Application\Model\SurveyModel::class);
+        return new SurveyController($surveyModel);
     }
 }
