1336189b7d4b3c6d25782e74d2f8ff8ae91910ee
Ali Martin Dev <alistairmartin@live.com>
2016-11-30 16:41:38 +0000

    S1 - removed redundant view template

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -93,7 +93,6 @@ return [
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
-            'application/login/login' => __DIR__ . '/../view/application/user-authentication/login.phtml',
         ],
         'template_path_stack' => [
             __DIR__ . '/../view',
