45076afedbd908858cfb126548cbe44640add1a1
Jess <j.j.isaacs@hotmail.co.uk>
2016-11-30 13:35:58 +0000

    fixed BuilderControlFactory param after merge

--- module/Application/src/Factory/BuilderControllerFactory.php
+++ module/Application/src/Factory/BuilderControllerFactory.php
@@ -5,7 +5,7 @@ namespace Application\Factory;
 class BuilderControllerFactory
 {
     public function __invoke($sm) {
-        $user = $sm->get(\Application\Model\User::class);
+        $user = $sm->get(\Application\Model\UserModel::class);
         return new \Application\Controller\BuilderController($user);
     }
 }
