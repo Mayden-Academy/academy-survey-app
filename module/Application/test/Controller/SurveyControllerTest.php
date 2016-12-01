a6a77c20abe3b8f7bb12d40c1f5daec60d7e2054
Luke Winsley <lukewinsley@gmail.com>
2016-12-01 16:55:03 +0000

    creating unit tests for survey controller

--- module/Application/test/Controller/SurveyControllerTest.php
+++ module/Application/test/Controller/SurveyControllerTest.php
@@ -13,14 +13,64 @@ use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 
 class SurveyControllerTest extends AbstractHttpControllerTestCase
 {
-public function testCreateActionGood () {
+    private $goodInput = [
+        'survey_name' => 'survey name',
+        'questions' => [
+            1 => [
+                'question_order' => 1,
+                'question_text' => 'aaaaaaaaaaaaaaa',
+                'question_type' => 'text',
+                'required' => '0',
+                'options' => [
+                    'dummy'
+                ]
+            ]
+        ]
+    ];
 
+    public function testValidateDataGood() {
 
+        SurveyController::validateData($this->goodInput);
+        $output = SurveyController::validateData($this->goodInput);
+        $this->assertTrue($output);
+    }
 
+    public function testValidateDataNoSurveyName() {
 
+        $noNameInput = $this->goodInput;
+        unset($noNameInput['survey_name']);
+        SurveyController::validateData($this->goodInput);
+        $output = SurveyController::validateData($this->goodInput);
+        $this->assertTrue($output);
 
-    $surveyController = new SurveyController();
-    $_SESSION['userAuth'] = 'hello';
+    }
+
+    public function testValidateDataNoQuestions() {
+
+        $noNameInput = $this->goodInput;
+        unset($noNameInput['questions']);
+        SurveyController::validateData($this->goodInput);
+        $output = SurveyController::validateData($this->goodInput);
+        $this->assertTrue($output);
+    }
 
 }
-}
+
+
+
+
+
+//if (
+//    !empty($data['survey_name']) &&
+//    !empty($data['questions']) &&
+//    is_array($data['questions'])
+//) {
+//    foreach ($data['questions'] as $question) {
+//        if(
+//            !empty($question['question_order']) &&
+//            !empty($question['question_text']) &&
+//            !empty($question['question_type']) &&
+//            !empty($question['required']) &&
+//            !empty($question['options']) &&
+//            is_array($question['options']) &&
+//            count($question['options']) > 0
