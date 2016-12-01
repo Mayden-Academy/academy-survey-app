615fc73f76eaa945e57abc7f8bc9ef42b3591121
Ben Morris <ben.morris@mayden.co.uk>
2016-12-01 14:19:41 +0000

    S1 - Docbloc UserAuthenticationController

--- module/Application/src/Controller/UserAuthenticationController.php
+++ module/Application/src/Controller/UserAuthenticationController.php
@@ -11,11 +11,27 @@ class UserAuthenticationController extends AbstractActionController
 {
     private $userModel;
 
+    /**
+     * UserAuthenticationController constructor.
+     *
+     * @param UserModel $userModel An instance of UserModel
+     */
     public function __construct(UserModel $userModel)
     {
         $this->userModel = $userModel;
     }
 
+    /**
+     * Controller action for routes:
+     * '/'
+     * '/login'
+     *
+     * Checks current user in session against the database
+     * Redirects to '/account/get' on success
+     * Redirects to '/login' on failure
+     *
+     * @return \Zend\Http\Response\ViewModel
+     */
     public function indexAction() {
         if(!empty($_SESSION['userAuth'])) {
             if($this->userModel->validateToken($_SESSION['userAuth'], $_SESSION['id']))
@@ -33,11 +49,17 @@ class UserAuthenticationController extends AbstractActionController
         }
     }
 
+    /**
+     * Controller action for routes:
+     * '/logout'
+     *
+     * Destroys the session
+     * Redirects to '/login'
+     *
+     * @return \Zend\Http\Response\ViewModel
+     */
     public function logoutAction() {
         session_destroy();
-        return $this->redirect()->toRoute('login',
-            ['controller'=>UserAuthenticationController::class,
-                'action' => 'index',
-                'params' =>'hello']);
+        return $this->redirect()->toRoute('login');
     }
 }
