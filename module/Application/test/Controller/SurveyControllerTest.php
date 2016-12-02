0c36180d7db7269664b84c8f1ef9d6331a5d2eed
Luke Winsley <lukewinsley@gmail.com>
2016-12-02 10:26:56 +0000

    created unit tests for survey controller

--- module/Application/test/Controller/SurveyControllerTest.php
+++ module/Application/test/Controller/SurveyControllerTest.php
@@ -7,20 +7,20 @@
 
 namespace ApplicationTest\Controller;
 
-use Application\Controller\SurveyController;
-use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use \Application\Controller\SurveyController;
+use \PHPUnit\Framework\TestCase;
+include __DIR__ . '/../../../../vendor/autoload.php';
 
-
-class SurveyControllerTest extends AbstractHttpControllerTestCase
+class SurveyControllerTest extends TestCase
 {
     private $goodInput = [
         'survey_name' => 'survey name',
         'questions' => [
-            1 => [
+            0 => [
                 'question_order' => 1,
                 'question_text' => 'aaaaaaaaaaaaaaa',
                 'question_type' => 'text',
-                'required' => '0',
+                'required' => '1',
                 'options' => [
                     'dummy'
                 ]
@@ -35,42 +35,67 @@ class SurveyControllerTest extends AbstractHttpControllerTestCase
         $this->assertTrue($output);
     }
 
-    public function testValidateDataNoSurveyName() {
-
-        $noNameInput = $this->goodInput;
-        unset($noNameInput['survey_name']);
-        SurveyController::validateData($this->goodInput);
-        $output = SurveyController::validateData($this->goodInput);
-        $this->assertTrue($output);
-
+    public function testValidateDataNoSurveyName()
+    {
+        $modifiedInput = $this->goodInput;
+        unset($modifiedInput['survey_name']);
+        SurveyController::validateData($modifiedInput);
+        $output = SurveyController::validateData($modifiedInput);
+        $this->assertFalse($output);
     }
 
-    public function testValidateDataNoQuestions() {
+    public function testValidateDataNoQuestions()
+    {
+        $modifiedInput = $this->goodInput;
+        unset($modifiedInput['questions']);
+        SurveyController::validateData($modifiedInput);
+        $output = SurveyController::validateData($modifiedInput);
+        $this->assertFalse($output);
+    }
 
-        $noNameInput = $this->goodInput;
-        unset($noNameInput['questions']);
-        SurveyController::validateData($this->goodInput);
-        $output = SurveyController::validateData($this->goodInput);
-        $this->assertTrue($output);
+    public function testValidateDataNoQuestionOrder()
+    {
+        $modifiedInput = $this->goodInput;
+        unset($modifiedInput['questions'][0]['question_order']);
+        SurveyController::validateData($modifiedInput);
+        $output = SurveyController::validateData($modifiedInput);
+        $this->assertFalse($output);
+    }
+
+    public function testValidateDataNoQuestionText()
+    {
+        $modifiedInput = $this->goodInput;
+        unset($modifiedInput['questions'][0]['question_text']);
+        SurveyController::validateData($modifiedInput);
+        $output = SurveyController::validateData($modifiedInput);
+        $this->assertFalse($output);
+    }
+
+    public function testValidateDataNoQuestionType()
+    {
+        $modifiedInput = $this->goodInput;
+        unset($modifiedInput['questions'][0]['question_type']);
+        SurveyController::validateData($modifiedInput);
+        $output = SurveyController::validateData($modifiedInput);
+        $this->assertFalse($output);
+    }
+
+    public function testValidateDataNoQuestionRequired()
+    {
+        $modifiedInput = $this->goodInput;
+        unset($modifiedInput['questions'][0]['required']);
+        SurveyController::validateData($modifiedInput);
+        $output = SurveyController::validateData($modifiedInput);
+        $this->assertFalse($output);
+    }
+
+    public function testValidateDataNoQuestionOptions()
+    {
+        $modifiedInput = $this->goodInput;
+        unset($modifiedInput['questions'][0]['options']);
+        SurveyController::validateData($modifiedInput);
+        $output = SurveyController::validateData($modifiedInput);
+        $this->assertFalse($output);
     }
 
 }
-
-
-
-
-
-//if (
-//    !empty($data['survey_name']) &&
-//    !empty($data['questions']) &&
-//    is_array($data['questions'])
-//) {
-//    foreach ($data['questions'] as $question) {
-//        if(
-//            !empty($question['question_order']) &&
-//            !empty($question['question_text']) &&
-//            !empty($question['question_type']) &&
-//            !empty($question['required']) &&
-//            !empty($question['options']) &&
-//            is_array($question['options']) &&
-//            count($question['options']) > 0
